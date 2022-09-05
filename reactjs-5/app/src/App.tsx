73d977a3e8c0796cb970142d37221ef5da487ac7
gbvanrenswoude <gbvanrenswoude@gmail.com>
2022-09-05 22:33:26 +0200

    feat: 🎸 fix the chat app

--- reactjs-5/app/src/App.tsx
+++ reactjs-5/app/src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { UserContext } from './userContext';
 
 function App() {
+  const [users, setUsers] = useState({
+    Nicolas: true,
+    Mary: true,
+    Julia: true,
+    John: true,
+    Jorge: true,
+  });
+
+  // @ts-ignore
+  const randomizeOneUser = (users) => {
+    const names = Object.keys(users);
+    const random = Math.floor(Math.random() * names.length);
+    const newUsers = { ...users };
+    newUsers[names[random]] = !users[names[random]];
+    return newUsers;
+  };
+
+  const randomizeUsers = () => {
+    setUsers(randomizeOneUser);
+  };
+  useEffect(() => {
+    const interval = setTimeout(randomizeUsers, 5000);
+    return; // ???
+  }, [users]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // @ts-ignore
+    <UserContext.Provider value={{ users, setUsers }}>
+      <>
+        <UserList />
+        <UserStatus />
+        <ActionButton />
+      </>
+    </UserContext.Provider>
+  );
+}
+
+function UserList() {
+  // @ts-ignore
+  const { users, setUsers } = React.useContext(UserContext);
+  return (
+    <div style={{ padding: 20 }}>
+      {Object.keys(users).map((key) => (
+        <User name={key} status={users[key]} users={users} />
+      ))}
     </div>
   );
 }
 
+// @ts-ignore
+function User({ name, status, users }) {
+  const currentlyOnline = Object.keys(users).filter((key) => users[key]).length;
+  if (currentlyOnline === 1) {
+    return (
+      <p>
+        {`${name}: ${status ? 'ONLINE' : 'OFFLINE'} ${
+          status ? 'and I m all alone...' : ''
+        }`}
+      </p>
+    );
+  } else {
+    return <p>{`${name}: ${status ? 'ONLINE' : 'OFFLINE'}`}</p>;
+  }
+}
+
+// @ts-ignore
+function UserStatus() {
+  // @ts-ignore
+  const { users, setUsers } = React.useContext(UserContext);
+  const online = Object.keys(users).filter((key) => users[key]);
+  return <p>There are currently {online.length} users online</p>;
+}
+
+// @ts-ignore
+function ActionButton() {
+  // @ts-ignore
+  const { users, setUsers } = React.useContext(UserContext);
+
+  // @ts-ignore
+  const randomizeOneUser = (users) => {
+    const names = Object.keys(users);
+    const random = Math.floor(Math.random() * names.length);
+    const newUsers = { ...users };
+    newUsers[names[random]] = !users[names[random]];
+    return newUsers;
+  };
+  const randomizeUsers = () => {
+    setUsers(randomizeOneUser);
+  };
+  return (
+    // @ts-ignore
+    <button type='text' onClick={randomizeUsers}>
+      Randomize now!
+    </button>
+  );
+}
 export default App;
